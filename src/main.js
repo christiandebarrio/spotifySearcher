e6de29167f6b1622a9449c28be5e524b3f3c94f9
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-24 20:13:23 +0100

    show albums

--- src/main.js
+++ src/main.js
@@ -7,12 +7,12 @@ function onSubmit (event) {
     name: $('#artist').val()
   }
 
-  var request = $.get('https://api.spotify.com/v1/search?type=artist&query=' + artist.name)
+  var request = $.get('https://api.spotify.com/v1/search?type=artist&query=' + artist.name);
 
   function onSaveSuccess (response) {
     console.debug('BOOM', response);
     name: $('#artist').empty();
-    respondSpotify(response);
+    showArtists(response);
   }
 
   function onSaveFailure (err) {
@@ -25,12 +25,54 @@ function onSubmit (event) {
 
 $('#spotify-searcher').on('submit', onSubmit);
 
-function respondSpotify (artist_obj) {
+function showArtists (artist_obj) {
   $('#content-search').empty();
+  $('#content-search').append('<h1>Artists</h1>')
+  $('#content-search').append('<ul id="list">');
   artist_obj.artists.items.forEach(function (artist) {    
     if(artist.images.length !== 0) {
-      var html = '<li><aricle class="artist"><img src="' + artist.images[artist.images.length - 1].url + '"/> <span class="artist_name">' + artist.name + '</span></article></li>';
-      $('#content-search').append(html);
+      var image = '<div class="cover"><img src="' + artist.images[0].url + '"/></div>'
+      var name = '<div class="data-name"><h1>' + artist.name + '</h1></div>'
+      var html = '<li><article class="box"><a href="" class="link" data-id="' + artist.id + '">' + image + name + '</a></article></li>';
+      $('#list').append(html);
     };
   });
+  $('#content-search').append('</ul>');
+}
+
+function goArtistPage (event) {
+  event.preventDefault();
+  var artist = event.currentTarget.dataset.id;
+
+  var request = $.get('https://api.spotify.com/v1/artists/' + artist + '/albums');
+
+  function onSaveSuccess (response) {
+    console.debug('BOOM', response);
+    name: $('#artist').empty();
+    showAlbums(response);
+  }
+
+  function onSaveFailure (err) {
+    console.error(err.responseJSON);
+  }
+
+  request.done(onSaveSuccess);
+  request.fail(onSaveFailure); 
+}
+
+$('#content-search').on('click', '.link', goArtistPage);
+
+function showAlbums (albums_obj) {
+  $('#content-search').empty();
+  $('#content-search').append('<h1>Albums</h1>')
+  $('#content-search').append('<ul id="list">');
+  albums_obj.items.forEach(function (album) {
+    if(album.images.length !== 0) {
+      var image = '<div class="cover"><img src="' + album.images[0].url + '"/></div>'
+      var name = '<div class="data-name"><h1>' + album.name + '</h1></div>'
+      var html = '<li><article class="box"><a href="" class="link">' + image + name + '</a></article></li>';
+      $('#list').append(html);
+    };
+  });
+  $('#content-search').append('</ul>');
 }
